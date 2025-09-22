155569d476bd8da7a623df9d29abc6e092e36bf8
sekar120601 <aliazaleha16@gmail.com>
2025-09-22 11:22:40 +0700

    fitur 1

--- index.php
+++ index.php
@@ -17,7 +17,8 @@
     <p>Hari ini aku pelatihan dev ops gaes</p>
     <p>Bersama bapak bapak</p>
     <p>ayooo bisaaa lah</p>
-     <p>Fitur 2</p>
+    <p>Fitur 2</p>
+    <p>Fitur 3</p>
   </main>
 
   <footer>
